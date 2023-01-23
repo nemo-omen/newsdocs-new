c6916d1d685369b6f797d1d88e48198f7556b115
Jeff Caldwell <nemo.omen@gmail.com>
2023-01-23 07:59:50 -0600

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,31 @@
+<script>
+  import '../style.css';
+</script>
+
+<svelte:head>
+  <title>NewsDocs</title>
+</svelte:head>
+
+<header>
+  <div class="inner header-inner">
+    <a href="/" id="brand" aria-label="NewsDocs.org">
+      <img src="logo.svg" alt="NewsDocs" width="64" height="64" />
+    </a>
+    <nav>
+      <a href="/">Home</a>
+      <a href="/roster">Jail Logs</a>
+    </nav>
+  </div>
+</header>
+
+<main>
+  <div class="inner">
+    <slot />
+  </div>
+</main>
+
+<footer>
+  <div class="inner">
+    &copy; {new Date().getFullYear()} NewsDocs
+  </div>
+</footer>
